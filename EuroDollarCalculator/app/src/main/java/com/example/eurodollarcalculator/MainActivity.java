91125d76428bc3b7ac17b824022d5f039ab9e0db
Akash <akashgurram18@gmail.com>
2019-03-02 18:56:05 -0500

    updated the code

--- EuroDollarCalculator/app/src/main/java/com/example/eurodollarcalculator/MainActivity.java
+++ EuroDollarCalculator/app/src/main/java/com/example/eurodollarcalculator/MainActivity.java
@@ -7,65 +7,58 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{ //implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText dollartext;
     private EditText eurotext;
     private Button ConvertDtoE;
     private Button ConvertEtoD;
     private static final float conversion = (float) 0.87;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        dollartext = (EditText)findViewById(R.id.dollarstext);
+        dollartext = (EditText) findViewById(R.id.dollarstext);
         eurotext = (EditText) findViewById(R.id.eurostext);
         ConvertDtoE = (Button) findViewById(R.id.ConvertDtoE);
-        //ConvertDtoE.setOnClickListener(this);
-        ConvertDtoE.setOnClickListener(new View.OnClickListener() {   //method to do the registration from app to android runtime.
-            @Override                                //onclicklistener is an interface // an anonymous class
-            public void onClick(View v) {
+        ConvertDtoE.setOnClickListener(this);
 
-                if (dollartext.getText().toString().compareTo("")!=0)
-                {
-                    float dollar_ = new Float(dollartext.getText().toString()).floatValue();
-                    float euros_ = dollar_* conversion;
-                    eurotext.setText(new Float(euros_).toString());
-
-                }
-                else{
-
-                    Toast.makeText(getApplicationContext(),"Dollar Text field is empty" +
-                            ":Insert a valid Dollar Value",Toast.LENGTH_LONG).show();
-
-                }
-
-                //double dollars =  Double.valueOf(dollartext.getText().toString());
-                //double euros = dollars * 0.87 ;
-                //eurotext.setText(String.valueOf(euros));
-            }
-        });
 
         ConvertEtoD = (Button) findViewById(R.id.ConvertEtoD);
-        ConvertEtoD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (eurotext.getText().toString().compareTo("")!=0)
-                {
+        ConvertEtoD.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ConvertDtoE:
+                if (dollartext.getText().toString().compareTo("") != 0) {
+                    float dollar_ = new Float(dollartext.getText().toString()).floatValue();
+                    float euros_ = dollar_ * conversion;
+                    eurotext.setText(new Float(euros_).toString());
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "DollarTextfieldisempty" +
+                            ":InsertavalidDollarValue", Toast.LENGTH_LONG).show();
+
+                }
+            case R.id.ConvertEtoD:
+                if (eurotext.getText().toString().compareTo("") != 0) {
                     float euros_ = new Float(eurotext.getText().toString()).floatValue();
-                    float dollars_ = euros_*(1/conversion);
+                    float dollars_ = euros_ * (1 / conversion);
                     dollartext.setText(new Float(dollars_).toString());
 
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "EuroTextfieldisempty" +
+                            ":InsertavalidEuroValue", Toast.LENGTH_LONG).show();
+
+
                 }
 
-                else{
-
-                    Toast.makeText(getApplicationContext(),"Euro Text field is empty" +
-                            ":Insert a valid Dollar Value",Toast.LENGTH_LONG).show();
-
-                }
-            }
-        });
+        }
     }
 }
